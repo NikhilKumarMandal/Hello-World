0259d5c96d3c264d9016c82e1d3467b27a494d07
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-08-12 23:37:23 +0530

    write my first test case for user registration

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response, NextFunction } from "express";
 import logger from "./config/logger";
 import { HttpError } from "http-errors";
+import authRouter from "../src/routes/auth";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome to our application");
 });
+
+app.use("/auth", authRouter);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
---
+++ tests/users/register.spec.ts
@@ -0,0 +1,43 @@
+import request from "supertest";
+import app from "../../src/app";
+
+describe("POST /auth/register", () => {
+  describe("Give all path", () => {
+    it("should return the 201 status code", async () => {
+      // AAA
+
+      // Arrange
+      const userData = {
+        firstName: "Nikhil",
+        lastName: "kumar",
+        email: "nikhilkumar@gmail.com",
+        password: "strick",
+      };
+
+      //Act
+      const response = await request(app).post("/auth/register").send(userData);
+
+      // Assert
+      expect(response.statusCode).toBe(201);
+    });
+
+    it("should return valid json response", async () => {
+      // Arrange
+      const userData = {
+        firstName: "Nikhil",
+        lastName: "kumar",
+        email: "nikhilkumar@gmail.com",
+        password: "strick",
+      };
+
+      //Act
+      const response = await request(app).post("/auth/register").send(userData);
+
+      // Assert
+      expect(
+        (response.header as Record<string, string>)["content-type"]
+      ).toEqual(expect.stringContaining("json"));
+    });
+  });
+  describe("Sad path", () => {});
+});
