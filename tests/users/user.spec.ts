45b216f24a735171c5b92de32af3c2419c5d3fa5
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-08-23 18:28:38 +0530

    add test to exculde password field

--- tests/users/user.spec.ts
+++ tests/users/user.spec.ts
@@ -76,5 +76,32 @@ describe("POST /api/v1/auth/self", () => {
 
       expect((response.body as Record<string, string>).id).toBe(data.id);
     });
+
+    it("should not return the password fields", async () => {
+      // Arrange
+      const userData = {
+        firstName: "Nikhil",
+        lastName: "kumar",
+        email: "nikhilkumar@gmail.com",
+        password: "password",
+      };
+
+      const userRepository = connection.getRepository(User);
+      const data = await userRepository.save({
+        ...userData,
+        role: Roles.CUSTOMER,
+      });
+
+      const accessToken = jwks.token({ sub: String(data.id), role: data.role });
+
+      const response = await request(app)
+        .get("/api/v1/auth/self")
+        .set("Cookie", [`accessToken=${accessToken};`])
+        .send();
+
+      expect(response.body as Record<string, string>).not.toHaveProperty(
+        "password"
+      );
+    });
   });
 });
